2bd8aa24f9d5796f2d78a88968246cbd0c62cd8d
Danish iftikhar <danishiftikhar333@gmail.com>
2020-12-23 13:09:33 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
+import Details from "./Components/Details";
+import Cart from "./Components/Cart/Cart";
+import Default from "./Components/Default";
+import Modal from "./Components/Modal";
+function App() {
+  return (
+
+    <React.Fragment>
+      <Navbar/>
+
+      <Switch>
+        <Route path="/" exact  component={ProductList}/>
+        <Route path="/details" component={Details}/>
+        <Route path="/cart" component={Cart}/>
+        <Route  component={Default}/>
+      </Switch>  
+      <Modal /> 
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Details.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button'
+class Details extends Component {
+    state = {  }
+    render() { 
+        return ( 
+           <ProductConsumer>
+               {(value)=>{
+                   const {id,company,img,info,price,title,inCart}=value.detailProduct;
+                   return (
+                       <div className="conatiner py-5">
+                           {/* Title*/ }
+                           <div className="row">
+                               <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                  <h1>{title}  </h1> 
+                               </div>
+                           </div>
+                           <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                        <img src={img} className="image-fluid" alt="product"/>
+                                </div>
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                        <h2>model :{title}</h2>
+                                        <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                            Made By: <span className="text-uppercase">{company}</span>
+                                        </h4>
+                                        <h4 className="text-blue">
+                                            <strong>
+                                                price: <span>$</span>{price}
+                                            </strong>
+                                        </h4>
+                                        <p className="text-capitalize font-weight-bold mt-3 mb-0"> some info about product :</p>
+                                        <p className="text-muted lead">{info}</p>
+                                        <div className="">
+                                            <Link to="/">
+                                                <ButtonContainer>
+                                                    Go Back to Products
+                                                </ButtonContainer>
+                                            </Link>
+                                            <ButtonContainer cart 
+                                            disabled={inCart?true:false} 
+                                            onClick={()=>{
+                                                value.addToCart(id);
+                                                value.OpenModal(id);}}>
+                                                {inCart?"in Cart": "add to Cart"}
+                                            </ButtonContainer>
+
+                                        </div>
+
+                                </div>
+                           </div>
+                       </div>
+                   )
+                
+                }}
+           </ProductConsumer>
+         );
+    }
+}
+ 
+export default Details; 
